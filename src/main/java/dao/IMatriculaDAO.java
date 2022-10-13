ea93a054d126d7d3906ffdddb27bb796581e6dca
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2022-10-13 12:48:39 -0700

    DAW-08

---
+++ src/main/java/dao/IMatriculaDAO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.util.List;
+import modelos.Alumnos;
+import modelos.Cursos;
+
+/**
+ *
+ * @author Tecsup
+ */
+public interface IMatriculaDAO {
+    public List<Alumnos> buscarAlumnos(Alumnos alumno);
+    public List<Cursos> buscarCursos();
+    public boolean grabarMatricula(String[] datosMatricula,
+            String[] codigoCursos, String[] montos);
+}
